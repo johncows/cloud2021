aab26c001710ee1ce2e7865b6448fa045f126aad
王骏康 <18019915762@163.com>
2021-10-27 19:35:24 +0800

    代码提交

---
+++ cloud-message-provider/src/main/java/com/kk/MessageProviderApplication.java
@@ -0,0 +1,16 @@
+package com.kk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangjunkang
+ */
+@SpringBootApplication
+public class MessageProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageProviderApplication.class, args);
+    }
+
+}
